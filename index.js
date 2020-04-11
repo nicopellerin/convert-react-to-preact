427a0d083ab8cd0c6f6b7b357c634f270913f902
Nico Pellerin <hello@nicopellerin.io>
2020-04-10 21:01:16 -0400

    Cleans up pkg

--- index.js
+++ index.js
@@ -2,80 +2,110 @@
 const { program } = require("commander")
 const inquirer = require("inquirer")
 const fs = require("fs")
-const { spawnSync } = require("child_process")
+const execa = require("execa")
 const chalk = require("chalk")
 const emoji = require("node-emoji")
+const ora = require("ora")
 
-program.version("0.0.1").description("Converts React app to using Preact X")
-program.parse(process.argv)
+async function readPackageJson() {
+  const packageJson = await fs.promises.readFile("package.json", "utf-8")
+  const parsedPJ = JSON.parse(packageJson)
+  const dependencies = Object.keys(parsedPJ.dependencies)
+  return dependencies
+}
 
-const packageJson = fs.readFileSync("package.json", "utf-8")
-const parsedPJ = JSON.parse(packageJson)
+let dependencies
+readPackageJson().then((data) => (dependencies = data))
 
-// Prompt inquirer
-;(async () => {
+async function runInquirer() {
+  program.version("0.0.1").description("Converts React app to using Preact X")
+  program.parse(process.argv)
+
+  console.log(
+    chalk.bgGray.magentaBright("Converts React app to using Preact X")
+  )
+
+  const { packageManager, framework } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "packageManager",
+      message: "Please select a package manager",
+      choices: ["yarn", "npm"],
+    },
+    {
+      type: "list",
+      name: "framework",
+      message: "Please select your framework",
+      choices: ["Next.js", "Create-react-app"],
+    },
+  ])
+
+  return { packageManager, framework }
+}
+
+function getInstallArgs(packageManager, framework) {
+  let count = 1
+  let total = framework === "Next.js" ? "6" : "5"
+
+  const command = { add: "", remove: "" }
+  switch (packageManager) {
+    case "yarn":
+      command.add = "add"
+      command.remove = "remove"
+      break
+    case "npm":
+      command.add = "install -S"
+      command.remove = "uninstall"
+  }
+
+  return { count, total, command }
+}
+
+function doneInstall(start, spinner) {
+  const time = Date.now() - start
+  spinner.stop()
+  console.log(
+    "\n" + emoji.get("white_check_mark"),
+    chalk.bold.green("Done!"),
+    chalk.gray(`${time / 1000}s`)
+  )
+}
+
+async function runProcessing() {
   try {
-    console.log(
-      chalk.bgGrey.magentaBright("Convert your React app to use Preact X")
-    )
-
-    const { packageManager, framework } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "packageManager",
-        message: "Please select a package manager",
-        choices: ["yarn", "npm"],
-      },
-      {
-        type: "list",
-        name: "framework",
-        message: "Please select your framework",
-        choices: ["Next.js", "Create-react-app"],
-      },
-    ])
+    const { packageManager, framework } = await runInquirer()
+    let { count, total, command } = getInstallArgs(packageManager, framework)
 
     const start = Date.now()
-
-    let count = 1
-    let total = framework === "Next.js" ? "6" : "5"
-
-    const command = { add: "", remove: "" }
-    switch (packageManager) {
-      case "yarn":
-        command.add = "add"
-        command.remove = "remove"
-        break
-      case "npm":
-        command.add = "install -S"
-        command.remove = "uninstall"
-    }
 
     // Start
     console.log(
       "\n" + emoji.get("stopwatch"),
-      chalk.cyan(" Starting conversion...\n")
+      chalk.cyan(" Starting conversion...")
     )
+    const spinner = ora()
+    spinner.start()
 
     // Removes react
-    if (Object.keys(parsedPJ.dependencies).includes("react")) {
+    if (dependencies.includes("react")) {
       console.log(
         chalk.gray(`[${count}/${total}]`),
         chalk.bold.red(`Removing`),
         "react"
       )
       count++
-      spawnSync(packageManager, [command.remove, "react"])
+      await execa(packageManager, [command.remove, "react"])
     }
 
     // Removes react-dom
-    if (Object.keys(parsedPJ.dependencies).includes("react-dom")) {
+    if (dependencies.includes("react-dom")) {
       console.log(
         chalk.gray(`[${count}/${total}]`),
         chalk.bold.red(`Removing`),
         "react-dom"
       )
       count++
-      spawnSync(packageManager, [command.remove, "react-dom"])
+      await execa(packageManager, [command.remove, "react-dom"])
     }
 
     // Adds preact
@@ -85,7 +115,8 @@ const parsedPJ = JSON.parse(packageJson)
       "preact"
     )
     count++
-    spawnSync(packageManager, [command.add, "preact"])
+
+    await execa(packageManager, [command.add, "preact"])
 
     // Adds preact-compat alias
     console.log(
@@ -94,7 +125,7 @@ const parsedPJ = JSON.parse(packageJson)
       "preact-compat alias"
     )
     count++
-    spawnSync(packageManager, [
+    await execa(packageManager, [
       command.add,
       "preact-compat/react#1.x",
       "preact-compat/react-dom#1.x",
@@ -107,7 +138,7 @@ const parsedPJ = JSON.parse(packageJson)
       "preact-render-to-string"
     )
     count++
-    spawnSync(packageManager, [command.add, "preact-render-to-string"])
+    await execa(packageManager, [command.add, "preact-render-to-string"])
 
     // Adds preact-ssr-prepass alias
     if (framework === "Next.js") {
@@ -116,21 +147,16 @@ const parsedPJ = JSON.parse(packageJson)
         chalk.bold.blueBright(`Adding`),
         "preact-ssr-prepass alias"
       )
-      spawnSync(packageManager, [
+      await execa(packageManager, [
         command.add,
         "react-ssr-prepass@npm:preact-ssr-prepass",
       ])
     }
 
-    // Done
-    const time = Date.now() - start
-
-    console.log(
-      "\n" + emoji.get("white_check_mark"),
-      chalk.bold.green("Done!"),
-      chalk.gray(`${time / 1000}s`)
-    )
-  } catch (error) {
-    console.error(chalk.red("Something wrong occured!"))
+    doneInstall(start, spinner)
+  } catch (err) {
+    console.error(chalk.red("Something wrong occured!", err))
   }
-})()
+}
+
+runProcessing()
